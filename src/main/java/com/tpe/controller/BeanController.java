38a2c2c6dd3e3c1e7b11b2a0adf7726e747eae53
hakkioz22 <hakkioz@outlook.com>
2023-03-04 16:26:20 +0300

    04/03/2023 - Spring Security(Project Done)

--- src/main/java/com/tpe/controller/BeanController.java
+++ src/main/java/com/tpe/controller/BeanController.java
@@ -4,6 +4,8 @@ package com.tpe.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,11 @@ public class BeanController {
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
     public Map<String,String> getBeans(){
+
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        System.out.println("Current username: " + userDetails.getUsername() + " password: " +userDetails.getPassword() + userDetails.getAuthorities());
+
         String[] beanNames = applicationContext.getBeanDefinitionNames();
         Map<String,String> map = new HashMap<>();
 
